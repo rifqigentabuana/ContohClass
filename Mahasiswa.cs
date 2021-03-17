38ad4a08c2ae55ad60efca6bd7b9f15f23d6d459
rifqi <rifqigentabuana@students.amikom.ac.id>
2021-03-18 01:44:44 +0700

    commit

---
+++ Mahasiswa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjectTest01
+
+{
+	public class Mahasiswa
+	{
+		// properties
+		public string Nim { get; set; }
+		public string Nama { get; set; }
+		public float Ipk { get; set; }
+
+		// method
+		public void Registrasi()
+		{
+			Console.WriteLine("Nama: {0}", Nama);
+			Console.WriteLine("Nim: {0}", Nim);
+			Console.WriteLine("Ipk: {0}", Ipk);
+			Console.WriteLine("\nTelah melakukan registrasi");
+
+		}
+		public void IsiKrs()
+		{
+			Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTest01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Mahasiswa
+            Mahasiswa aing = new Mahasiswa();
+            Mahasiswa koe = new Mahasiswa();
+
+            // setting nilai properties objek aing dan koe
+            aing.Nim = "20.11.3334";
+            aing.Nama = "Jono";
+            aing.Ipk = 3.82f;
+
+            koe.Nim = "20.11.3333";
+            koe.Nama = "Genta";
+            koe.Ipk = 3.85f;
+
+            // memanggil method registrasi dan isi krs
+            aing.Registrasi();
+            aing.IsiKrs();
+
+            koe.Registrasi();
+            koe.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
